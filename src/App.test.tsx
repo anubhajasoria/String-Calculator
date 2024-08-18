101fceed18d59f87d36f41505a71299f31b8ddc7
Anubha Jasoria <anubhajasoria@Anubhas-Laptop.local>
2024-08-18 16:38:42 +0530

    Test - should return the number itself for a single number

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -7,4 +7,8 @@ describe("StringCalculator", () => {
   it("should return 0 for an empty string", () => {
     expect(add("")).toBe(0);
   });
+
+  it("should return the number itself for a single number", () => {
+    expect(add("1")).toBe(1);
+  });
 });
--- src/helper/Logic.ts
+++ src/helper/Logic.ts
@@ -1,5 +1,7 @@
 export function add(numbers: string): number | string {
   if (numbers === "") return 0;
 
+  if (numbers.length === 1) return parseInt(numbers);
+
   return "";
 }
