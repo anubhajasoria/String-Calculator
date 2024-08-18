0df130e14ab14ba392085cbb73819222ad1bd5b2
Anubha Jasoria <anubhajasoria@Anubhas-Laptop.local>
2024-08-18 17:34:29 +0530

    tailwind and Ui setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,48 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React, { useState } from "react";
 import { add } from "./helper/Logic";
 
-function App() {
-  return <div className="App">String Calculator</div>;
-}
+const App: React.FC = () => {
+  const [input, setInput] = useState("");
+  const [result, setResult] = useState<number | null>(null);
+  const [error, setError] = useState<string | null>(null);
+
+  const handleCalculate = () => {
+    try {
+      const sum = add(input);
+      setResult(sum);
+      setError(null);
+    } catch (err) {
+      setError((err as Error).message);
+      setResult(null);
+    }
+  };
+
+  return (
+    <div className="h-screen bg-gradient-to-r from-gray-500 to-gray-600 flex flex-col items-center justify-center">
+      <p className="">String Calculator</p>
+      <div className="flex flex-col justify-start mt-4">
+        <input
+          type="text"
+          value={input}
+          className="p-2 rounded-xl "
+          onChange={(e) => setInput(e.target.value)}
+          placeholder="Enter numbers"
+        />
+        <button
+          className="rounded-xl bg-indigo-500 text-white py-2 mt-4"
+          onClick={handleCalculate}
+        >
+          Calculate
+        </button>
+        {error && <p>{error}</p>}
+        {result !== null && !error && (
+          <p className="mt-4">
+            Result: <strong>{result}</strong>
+          </p>
+        )}
+      </div>
+    </div>
+  );
+};
 
 export default App;
--- src/helper/Logic.ts
+++ src/helper/Logic.ts
@@ -1,4 +1,4 @@
-export function add(numbers: string): number | string {
+export function add(numbers: string): number {
   if (numbers === "") return 0;
 
   if (numbers.length === 1) return parseInt(numbers);
