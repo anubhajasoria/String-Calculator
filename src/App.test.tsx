94f547b8180cd264d5b8ac682e83c2e1aabdb209
Anubha Jasoria <anubhajasoria@Anubhas-Laptop.local>
2024-08-18 16:35:03 +0530

    Test - return 0 for an empty string

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,3 +1,10 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
+import { add } from "./helper/Logic";
+
+describe("StringCalculator", () => {
+  it("should return 0 for an empty string", () => {
+    expect(add("")).toBe(0);
+  });
+});
---
+++ src/helper/Logic.ts
@@ -0,0 +1,5 @@
+export function add(numbers: string): number | string {
+  if (numbers === "") return 0;
+
+  return "";
+}
