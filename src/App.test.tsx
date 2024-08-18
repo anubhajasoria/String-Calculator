c2ecb5e1a2d70d357a974d65c10fe6d0cf6bbc72
Anubha Jasoria <anubhajasoria@Anubhas-Laptop.local>
2024-08-18 16:51:52 +0530

    Test - should return the sum of two numbers with \n and ,

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -11,4 +11,8 @@ describe("StringCalculator", () => {
   it("should return the number itself for a single number", () => {
     expect(add("1")).toBe(1);
   });
+
+  it("should return the sum of two numbers", () => {
+    expect(add("15")).toBe(6);
+  });
 });
--- src/helper/Logic.ts
+++ src/helper/Logic.ts
@@ -3,5 +3,11 @@ export function add(numbers: string): number | string {
 
   if (numbers.length === 1) return parseInt(numbers);
 
-  return "";
+  //removing \n and , from the input string
+  let str = numbers.replace(/[\\n,]/g, "");
+
+  return str
+    .split("")
+    .map((e) => parseInt(e))
+    .reduce((a, b) => a + b);
 }
